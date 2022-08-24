736c3dfa38f737f14a5a8d3b57fb25f952ef2c41
rashidya <rashmiliyanage2000@gmail.com>
2022-08-24 17:36:01 +0530

    Done Customer save

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 4000
 
-const url = 'mongodb://localhost/facebook'
+const url = 'mongodb://127.0.0.1/facebook'
 mongoose.connect(url,{ useNewUrlParser: true })
-const con = mongoose.connection
+const connection = mongoose.connection
 
+const User = require('./routes/users')
 
-con.on("open", () => {
+app.use(express.json())
+
+app.use('/users',User)
+
+connection.on("open", () => {
     console.log('MongoDB Connected!')
 })
+
+
 
 app.listen(port, () => {
     console.log(`App is listening on port -  ${port}`);
 })
+
+
 
